73b3a53fa1bad76b8542e3e5a1001c5dfb85cba0
Mikhail <belalovmikhail@gmail.com>
2023-05-01 13:39:48 +0300

    Add new Class

--- src/main/java/ProjectJS/MainClass.java
+++ src/main/java/ProjectJS/MainClass.java
@@ -1,4 +1,14 @@
 package ProjectJS;
 
 public class MainClass {
+    int id;
+    String description;
+    int amount;
+    int percent;
+    public MainClass(int id, int RAM, String VideoCard, String color) {
+        this.id = id;
+        this.description = description;
+        this.amount = amount;
+        this.percent = percent;
+    }
 }
