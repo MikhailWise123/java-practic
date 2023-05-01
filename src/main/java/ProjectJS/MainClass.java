769b6cd747009a56155f4a860ba87269a6843285
Mikhail <belalovmikhail@gmail.com>
2023-05-01 13:50:59 +0300

    modified

--- src/main/java/ProjectJS/MainClass.java
+++ src/main/java/ProjectJS/MainClass.java
@@ -1,5 +1,7 @@
 package ProjectJS;
 
+import lesson6.LAPTOP;
+
 public class MainClass {
     int id;
     String description;
@@ -10,5 +12,23 @@ public class MainClass {
         this.description = description;
         this.amount = amount;
         this.percent = percent;
+    }
+    public MainClass(int id) {
+        this.id = id;
+    }
+    @Override
+    public String toString() {
+        return String.format("%d %s %d %d", id, description, amount, percent);
+
+    }
+    @Override
+    public boolean equals(Object o){
+        var t = (MainClass) o;
+        return id == t.id;
+    }
+
+    @Override
+    public int hashCode(){
+        return id;
     }
 }
--- src/main/java/ProjectJS/ToyInfo.java
+++ src/main/java/ProjectJS/ToyInfo.java
@@ -1,4 +1,26 @@
 package ProjectJS;
 
+import lesson6.LAPTOP;
+
 public class ToyInfo {
+    public static void main(String[] args) {
+        MainClass toy1 = new MainClass(1);
+        toy1.description = "мягкая игрушка";
+        toy1.amount = 1;
+        toy1.percent = 1;
+
+        MainClass toy2 = new MainClass(2);
+        toy2.description = "машинка";
+        toy2.amount = 2;
+        toy2.percent = 2;
+
+        MainClass toy3 = new MainClass(3);
+        toy3.description = "кукла";
+        toy3.amount = 3;
+        toy3.percent = 3;
+
+        System.out.println(toy1);
+        System.out.println(toy2);
+        System.out.println(toy3);
+    }
 }
